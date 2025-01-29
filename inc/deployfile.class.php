eb9b52b8a753534751f80146e31c04f8b571ac36
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-01-29 02:16:19 -0300

    Update deployfile.class.php

--- inc/deployfile.class.php
+++ inc/deployfile.class.php
@@ -332,8 +332,7 @@ class PluginGlpiinventoryDeployFile extends PluginGlpiinventoryDeployPackageItem
         echo "</tr>";
 
         echo "<tr>";
-        echo "<th>" . __("Uncompress", 'glpiinventory') . "<img style='float:right' " .
-         "src='" . $fi_path . "/pics/uncompress.png' /></th>";
+        echo "<th>" . __("Uncompress", 'glpiinventory') . "<i style='float:right; color:gray;' class='ti ti-file-zip'></i></th>";
         echo "<td>";
         Html::showCheckbox(['name' => 'uncompress', 'checked' => $uncompress]);
         echo "</td>";
@@ -341,8 +340,7 @@ class PluginGlpiinventoryDeployFile extends PluginGlpiinventoryDeployPackageItem
 
         echo "<tr>";
         echo "<th>" . __("P2P", 'glpiinventory') .
-              "<img style='float:right' src='" . $fi_path .
-              "/pics/p2p.png' /></th>";
+              "<i style='float:right; color:gray;' class='ti ti-vector-triangle'></i></th>";
         echo "<td>";
         Html::showCheckbox(['name' => 'p2p', 'checked' => $p2p]);
         echo "</td>";
