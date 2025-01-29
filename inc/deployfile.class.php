d80137d7e8a4f32691dd45c016bc6a495be06e18
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2025-01-29 09:21:13 -0300

    Update deployfile.class.php

--- inc/deployfile.class.php
+++ inc/deployfile.class.php
@@ -303,7 +303,7 @@ class PluginGlpiinventoryDeployFile extends PluginGlpiinventoryDeployPackageItem
             switch ($request_data['value']) {
                 case "Computer":
                     echo "<input type='file' name='file' value='" .
-                    __("filename", 'glpiinventory') . "' />";
+                    __("filename", 'glpiinventory') . "' class='form-control' />";
                     echo " <i>" . $this->getMaxUploadSize() . "</i>";
                     break;
 
